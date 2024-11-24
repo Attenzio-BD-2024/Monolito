b4bc6a5c2d5831647f0a30131cc1a31b40ae9e42
Mónica <cortes.monica@correounivalle.edu.co>
2024-11-23 20:36:00 -0500

    models

---
+++ backend/attenzio/apps/question/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from apps.session.models import session
+
+# Create your models here.
+
+class question(models.Model):
+    id_question = models.IntegerField(primary_key=True)
+    question = models.TextField(max_length=400)
+    id_session = models.ForeignKey(session, on_delete=models.CASCADE)
+
+        
+    def __str__(self):
+        return f"{self.question} {self.id_question}" 
+
--- backend/attenzio/attenzio/settings.py
+++ backend/attenzio/attenzio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.user',
+    'apps.assistance',
+    'apps.options',
+    'apps.session',
+    'apps.question',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
